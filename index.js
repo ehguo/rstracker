a3b37657698ede09a307d695a007fbe1e6454d54
Edward Guo <edward.h.guo@gmail.com>
2019-04-08 23:42:40 -0700

    Refactoring

--- index.js
+++ index.js
@@ -1,3 +1,133 @@
+$().ready(() => {
+  const $root = $('#root');
+
+  const data = load('state') || {};
+
+  $root.append(RepeatableSection('Daily', daily, data));
+  $root.append(RepeatableSection('Weekly', weekly, data));
+  $root.append(RepeatableSection('Monthly', monthly, data));
+
+  startUpdatingTimers();
+  // startSaving();
+
+  // setInterval(() => {
+  //   const now = new Date();
+  //   console.log(getTimeUntilReset(now))
+  // }, 1000);
+});
+
+/******************************************************************************
+ * Time functions
+ */
+const resetTypes = { day: 'day', week: 'week', month: 'month' };
+
+const getDaysInMonth = now => new Date(now.getUTCFullYear(), now.getUTCMonth() + 1, 0).getDate();
+
+const getTimeUntilReset = now => {
+  const minutes = 60 - now.getUTCMinutes();
+  const hours = 23 - now.getUTCHours();
+  const daysWeek = now.getUTCDay() <= 2 ? 2 - now.getUTCDay() : 9 - now.getUTCDay();
+  const daysMonth = getDaysInMonth(now) - now.getUTCDate();
+
+  const dailyString = `${hours > 9 ? '' : '0'}${hours}:${minutes > 9 ? '' : '0'}${minutes} hours`;
+  const weeklyString = daysWeek > 1 ? `${daysWeek} days` : dailyString;
+  const monthlyString = daysMonth > 0 ? `${daysMonth} days` : dailyString;
+
+  return { dailyString, weeklyString, monthlyString };
+};
+
+const startUpdatingTimers = () => {
+  const updateTimers = () => {
+    const now = new Date();
+    const { dailyString, weeklyString, monthlyString } = getTimeUntilReset(now);
+  
+    $('#Daily').text(dailyString);
+    $('#Weekly').text(weeklyString);
+    $('#Monthly').text(monthlyString);
+  };
+
+  setInterval(updateTimers, 1000);
+};
+
+
+/******************************************************************************
+ * Storage functions
+ */
+const save = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+const load = key => JSON.parse(localStorage.getItem(key));
+
+
+/******************************************************************************
+ * Util functions
+ */
+const underscoreText = text => text.replace(/ /g, '_');
+const generateIdDOM = text => text.replace(/ |\(|\)|'/g, '');
+const generateWikiLink = item => `https://runescape.wiki/w/${underscoreText(item)}`;
+
+
+/******************************************************************************
+ * Render functions
+ */
+const Section = props => $('<section></section>', props);
+const Div = props => $('<div></div>', props);
+const H1 = props => $('<h1></h1>', props);
+const Span = props => $('<span></span>', props);
+const A = props => $('<a></a>', props);
+const Input = props => $('<input></input>', props);
+
+const RepeatableSection = (title, items, data) => {
+  const $section = Section({ class: 'section' });
+
+  const $header = Div({ class: 'header' });
+  const $title = H1({ class: 'title', text: title });
+  const $reset = Span({ class: 'reset', text: 'reset' });
+  const $timer = Span({ id: title, class: 'timer', text: '' });
+  $reset.on('click', handleReset);
+  $header.append($title, $reset, $timer);
+
+  const $items = Div({ class: 'items' });
+  items.forEach(item => {
+    const id = generateIdDOM(item);
+    const $item = Div({ id, class: 'item' });
+
+    const $checkbox = Input({ class: 'checkbox', type: 'checkbox', checked: data[item] });
+    const $link = A({
+      class: 'link',
+      href: generateWikiLink(item),
+      target: '_blank',
+      text: item
+    });
+    if (data[item]) $link.addClass('strikethrough');
+    $item.append($checkbox, $link);
+    $item.on('click', handleItemClick(id, item, data));
+
+    $items.append($item);
+  });
+
+  $section.append($header, $items);
+  return $section;
+};
+
+
+/******************************************************************************
+ * Event handlers
+ */
+const handleItemClick = (id, item, data) => (e) => {
+  e.stopPropagation(e);
+  if (e.target.className === 'link') return;
+  data[item] = !data[item];
+
+  const $item = $(`#${id}`);
+  $('.checkbox', $item).prop('checked', data[item]);
+  $('.link', $item).toggleClass('strikethrough', data[item]);
+
+  save('state', data);
+};
+
+
+/******************************************************************************
+ * Default values
+ */
 const daily = [
   'Arhein',
   'Battlestaff',
@@ -39,184 +169,20 @@ const monthly = [
   'Troll Invasion'
 ];
 
-const underscoreText = text => text.replace(/ /g, '_');
+// const handleClick = ($checkbox, $link, e) => {
+//   if (e.target.className === 'link') {
+//     return;
+//   }
+//   if (e.target.className === 'checkbox') {
+//     $link.toggleClass('strikethrough');
+//     return;
+//   }
+//   $checkbox.prop('checked', !$checkbox.prop('checked'));
+//   $link.toggleClass('strikethrough');
+// };
 
-const startUpdatingTimers = () => {
-  const getDaysInMonth = (month, year) => new Date(year, month, 0).getDate();
+// const handleReset = e => {
+//   $('.checkbox', $(e.target).closest('.section')).prop('checked', false);
+//   $('.link', $(e.target).closest('.section')).removeClass('strikethrough');
+// }
 
-  const updateTimer = () => {
-    const now = new Date();
-
-    const minute = now.getUTCMinutes();
-    const hour = now.getUTCHours();
-    const day = now.getUTCDay();
-    const date = now.getUTCDate();
-    const month = now.getUTCMonth();
-    const year = now.getUTCFullYear();
-    const daysInMonth = getDaysInMonth(month, year);
-
-    const minutesUntilReset = 60 - minute;
-    const hoursUntilReset = 23 - hour;
-    const daysUntilReset = day > 2 ? 9 - day : 2 - day;
-    const monthsUntilReset = daysInMonth - date;
-
-    const dailyResetString = `${hoursUntilReset}:${minutesUntilReset > 9
-      ? minutesUntilReset
-      : `0${minutesUntilReset}`} hours`;
-
-    const weeklyResetString = daysUntilReset >= 1
-      ? `${daysUntilReset} days`
-      : dailyResetString;
-    
-    const monthlyResetString = monthsUntilReset >= 1
-      ? `${monthsUntilReset} days`
-      : dailyResetString;
-
-    $('#Daily').text(dailyResetString);
-    $('#Weekly').text(weeklyResetString);
-    $('#Monthly').text(monthlyResetString);
-  };
-
-  setInterval(updateTimer, 1000);
-};
-
-const handleClick = ($checkbox, $link, e) => {
-  if (e.target.className === 'link') {
-    return;
-  }
-  if (e.target.className === 'checkbox') {
-    $link.toggleClass('strikethrough');
-    return;
-  }
-  $checkbox.prop('checked', !$checkbox.prop('checked'));
-  $link.toggleClass('strikethrough');
-};
-
-const handleReset = e => {
-  $('.checkbox', $(e.target).closest('.section')).prop('checked', false);
-  $('.link', $(e.target).closest('.section')).removeClass('strikethrough');
-}
-
-const renderSection = () => (
-  $('<section></section>', {
-    class: 'section'
-  })
-);
-
-const renderHeader = () => (
-  $('<div></div>', {
-    class: 'header'
-  })
-);
-
-const renderTitle = title => (
-  $('<h1></h1>', {
-    class: 'title',
-    text: title
-  })
-);
-
-const renderReset = () => (
-  $('<span></span>', {
-    class: 'reset',
-    text: 'reset'
-  })
-);
-
-const renderResetTimer = (title) => (
-  $('<span></span>', {
-    id: title,
-    class: 'timer',
-    text: ''
-  })
-);
-
-const renderItem = () => (
-  $('<div></div>', {
-    class: 'item'
-  })
-);
-
-const renderCheckbox = (item, checked) => (
-  $('<input></input>', {
-    id: `${item}`,
-    type: 'checkbox',
-    class: 'checkbox',
-    checked
-  })
-);
-
-const renderLink = text => (
-  $('<a></a>', {
-    class: 'link',
-    text,
-    href: `https://runescape.wiki/w/${underscoreText(text)}`,
-    target: '_blank'
-  })
-);
-
-const Section = (title, items, data) => {
-  const $section = renderSection();
-  const $header = renderHeader();
-  const $title = renderTitle(title);
-  const $reset = renderReset();
-  const $resetTimer = renderResetTimer(title);
-
-  $reset.on('click', handleReset);
-
-  $header.append($title);
-  $header.append($reset);
-  $header.append($resetTimer);
-  $section.append($header);
-
-  items.forEach(item => {
-    const $item = renderItem();
-    const $checkbox = renderCheckbox(item, data[item]);
-    const $link = renderLink(item);
-
-    if (data[item]) {
-      $link.addClass('strikethrough');
-    }
-
-    $item.append($checkbox);
-    $item.append($link);
-    $item.on('click', (e) => handleClick($checkbox, $link, e));
-
-    $section.append($item);
-  });
-
-  return $section;
-};
-
-const startSaving = () => {
-  const save = () => {
-    const $items = $('.checkbox');
-    const data = Object.values($items)
-      .filter(item => item.id)
-      .reduce((result, { id, checked }) => {
-        result[id] = checked;
-        return result;
-      }, {});
-    const jsoned = JSON.stringify(data);
-    localStorage.setItem('state', jsoned);
-  };
-  setInterval(save, 1000);
-};
-
-const load = () => {
-  const state = localStorage.getItem('state');
-  return JSON.parse(state);
-};
-
-$().ready(() => {
-  const $root = $('#root');
-
-  const data = load() || {};
-
-  $root.append(Section('Daily', daily, data));
-  $root.append(Section('Weekly', weekly, data));
-  $root.append(Section('Monthly', monthly, data));
-
-  startUpdatingTimers();
-  startSaving();
-});
